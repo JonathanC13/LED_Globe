d342216cd3a0a2e3c2327e9a885b99ecf97c1017
Denis Chupin <denischupin@LABS.SCE.CARLETON.CA>
2017-11-13 14:28:26 -0500

    GUI: Added visibility rules for buttons and constraints of layout

--- UIapplication/app/src/main/java/m9/sysc3010/uiapplication/MainActivity.java
+++ UIapplication/app/src/main/java/m9/sysc3010/uiapplication/MainActivity.java
@@ -3,6 +3,7 @@ package m9.sysc3010.uiapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +15,12 @@ public class MainActivity extends AppCompatActivity {
 
     /*Method to send the request to start the Globe*/
     public void sendStartRequest(View view) {
-        // Do something in response to Start button
+        // Do something in response to Start button RPM 45, 0602 - 0601
+        //Make other buttons visible after click of start
+        Button stopButton=(Button) findViewById(R.id.button_stop);
+        Button inputButton=(Button) findViewById(R.id.button_input);
+        inputButton.setVisibility(View.VISIBLE);
+        stopButton.setVisibility(View.VISIBLE);
     }
 
     /*Method to send the request to stop the Globe*/
