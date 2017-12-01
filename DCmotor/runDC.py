5b2cf1644cf4aaa76fe8603d1103f56086fed9a7
JonathanC13 <jonhschan@hotmail.com>
2017-12-01 14:26:05 -0500

    changed motor, so changed values

--- DCmotor/runDC.py
+++ DCmotor/runDC.py
@@ -8,13 +8,10 @@ import os, sys
 from MotorStub import *
 
 # When testing with stub, comment GPIO and pwm functions.
-#GPIO.setmode(GPIO.BOARD)
-
-#GPIO.setup(12, GPIO.OUT)
-
-#pwm = GPIO.PWM(12, 1000) # Arbitrary
-
-#pwm.start(0) 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+pwm = GPIO.PWM(12, 1000) # Arbitrary
+pwm.start(0)
 
 # -----
 
@@ -25,112 +22,109 @@ class runDC:
     # gets user desired RPSec then converts to duty cycle
     def userRPStoDuty(self, RPS):
 
-        #current DC motor: #:29DCM28 - 8500 RPM. 141 RPSec @9V (no load)
-        rpsMAX = 141
-        dcV = 9        
+        #DC motor: #:29DCM28 - 8500 RPM. 141 RPSec @9V (no load)
+        # rpsMAX = 141
+
+        #currentDC motor: - 13800 RPM. 230 RPSec @3V (no load)
+        rpsMAX = 230
+        dcV = 3
         if(RPS < 0):
             RPS = 0
-        elif (RPS >= 141):
-            RPS = 141
-		
+        elif (RPS >= rpsMAX):
+            RPS = rpsMAX
+
         #(Theo aV/Vmax) x rpsMAX = desired rps
-        
-        
+
         #RPS to duty cycle (%)
         duty = (float(RPS)*100.0/float(rpsMAX))
-        
-        
-        #print ("DC motor will reach " + str(RPS) + " RPS momentarily")
-        #print ("Duty cycle applied: " + str(duty) + "%")
-        
-        #Theo average V = Vmax x duty
-        Tav = float(dcV * (duty/100.0))
-        
 
-        #print ("Theoretical aveage voltage applied: " + str(Tav) + "V, of max " + str(dcV) + "V")
+        #Theo average V = Vmax x duty
 
         #duty-pwm relation equation. duty = (pwm x 100)/T
 
         return duty
 
     # For testing added parameters: inRPS and stub instance
-    def applyDuty(self, inRPS, stubInst):
-
-        stub = stubInst
+    # def applyDuty(self, inRPS, stubInst):
+    def applyDuty(self):
+        #stub = stubInst
+        #UpperLim = 141     9 DC motor, 8500 RPM = 141 RPS
+        UpperLim = 230    # 3 DC motor, 13800 RPM = 230 revolutions per second
         try:
             while True:
 			#RPS read from a text file that is written to by the TFTP server.
-	
-		#Input Tests ----
-		#RPS changes
-                RPS = inRPS
+
+                #Input Tests ----
+                #RPS changes
+
+                #RPS = inRPS
                 # ---
 
-		# Actual Inputs ---	
-		#f = open('/home/pi/Desktop/RPS','r')
-		#RPS = f.read()
-		# -----
-		
-                if (RPS == -1):
+                # Actual Inputs ---
+                f = open('/home/pi/Desktop/RPS','r')
+                RPS = f.read()
+		        # -----
+
+                if (RPS == -1): # If file has -1, end motor
                     fRPS = 0
 
                     dutyCalc = self.userRPStoDuty(fRPS)
-                    
+
                     #Stub for motor ---
-                    stub.stubPWM(dutyCalc)
+                    #stub.stubPWM(dutyCalc)
                     # ---
 
                     # Actual motor control ---
-                    #pwm.ChangeDutyCycle(dutyCalc)
+                    pwm.ChangeDutyCycle(dutyCalc)
                     # ---
-                    
+
                     break
-                elif(RPS < 0):
+                elif(RPS < 0): #Cannot have rps of less than 0
                     RPS = 0
-                elif (RPS >= 141):
-                    RPS = 141
+                elif (RPS >= UpperLim):
+                    RPS = UpperLim
 
                 fRPS = float(RPS)
-				
-		# -----
-                #f.close()
+
+		              # -----
+                f.close()
                 # ----
                 dutyCalc = self.userRPStoDuty(fRPS)
 
 				#Stub for motor ---
-                stub.stubPWM(dutyCalc)
-						# ---
-						
+                #stub.stubPWM(dutyCalc)
+				# ---
+
 				# Actual motor control ---
-						#pwm.ChangeDutyCycle(dutyCalc)
-						# ---
-                time.sleep(2)
+                pwm.ChangeDutyCycle(dutyCalc)
+                # ---
+                time.sleep(2)   #Wait 2 seconds then read the speed file again
 
                 #break and end after 10 seconds
                 #time.sleep(10)
-                
+
                 # for testing, break after one input
-                break
-                
-        except KeyboardInterrupt:
-            pass
-        except ValueError:
-            print ("Could not convert data to an integer.")
-        except:
-            print ("Unexpected error:", sys.exc_info()[0])
+                #break
+
+    except KeyboardInterrupt:
+        pass
+    except ValueError:
+        print ("Could not convert data to an integer.")
+    except:
+        print ("Unexpected error:", sys.exc_info()[0])
 
 	# When testing with stub, comment GPIO and pwm functions.
-        #pwm.stop()
+    pwm.stop()
 
-        #GPIO.cleanup()
+    GPIO.cleanup()
 
         # -----
-        return;
-        
+    return;
+
 
     #def __init__(self, userRPS):
         #self.RPS = userRPS
-    
+
 #-- cmd line args
 #parser = argparse.ArgumentParser(description = 'Enter a integer for 0 to 141.')
 #parser.add_argument('integers', metavar='N', type=int, nargs='?', default='0',
